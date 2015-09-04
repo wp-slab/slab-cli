098d74878f5096c8304604503f66b1fd2bff7a13
Luke Lanchester <luke@lukelanchester.com>
2015-09-04 07:55:25 +0100

    Split functions into namespaced method

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Slab\Cli;
+
+/**
+ * Initialize Slab CLI
+ *
+ * @param Slab\Core\Application
+ * @return void
+ **/
+function slab_cli_init($slab) {
+
+	$slab->autoloader->registerNamespace('Slab\Cli', SLAB_CLI_DIR . 'src');
+
+	$slab->singleton('Slab\Cli\CommandCollection', function($app){
+		$collection = new CommandCollection($app);
+		do_action('slab_cli_commands', $collection);
+		return $collection;
+	});
+
+	$slab->alias('commands', 'Slab\Cli\CommandCollection');
+
+}
+
+
+/**
+ * Initialize Slab Router
+ *
+ * @param Slab\Cli\CommandCollection
+ * @return void
+ **/
+function slab_cli_default_commands($commands) {
+
+	$commands->resolve('Slab\Cli\Commands\ListCommandsCommand');
+	// $commands->addCommand(new Slab\Cli\Command('help'));
+	// $commands->addCommand(new Slab\Cli\Command('cron'));
+
+}
--- index.php
+++ index.php
@@ -19,30 +19,10 @@ define('SLAB_CLI_DIR', plugin_dir_path(__FILE__));
 define('SLAB_CLI_URL', plugin_dir_url(__FILE__));
 
 
+// Includes
+include SLAB_CLI_DIR . 'functions.php';
+
+
 // Hooks
-add_action('slab_init', 'slab_cli_init');
-add_action('slab_cli_commands', 'slab_cli_default_commands');
-
-
-// Init
-function slab_cli_init($slab) {
-
-	$slab->autoloader->registerNamespace('Slab\Cli', SLAB_CLI_DIR . 'src');
-
-	$slab->singleton('Slab\Cli\CommandCollection', function($app){
-		$collection = new Slab\Cli\CommandCollection($app);
-		do_action('slab_cli_commands', $collection);
-		return $collection;
-	});
-
-	$slab->alias('commands', 'Slab\Cli\CommandCollection');
-
-}
-
-function slab_cli_default_commands($commands) {
-
-	$commands->resolve('Slab\Cli\Commands\ListCommandsCommand');
-	// $commands->addCommand(new Slab\Cli\Command('help'));
-	// $commands->addCommand(new Slab\Cli\Command('cron'));
-
-}
+add_action('slab_init', 'Slab\Cli\slab_cli_init');
+add_action('slab_cli_commands', 'Slab\Cli\slab_cli_default_commands');
